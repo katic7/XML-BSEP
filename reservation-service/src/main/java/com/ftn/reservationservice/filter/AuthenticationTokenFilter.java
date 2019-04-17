5f6febd280d3ed82c5f6242c43ad70cefc1ac317
Nemanja <nemanjadimsic6@gmail.com>
2019-04-17 03:27:33 -0700

    AuthenticationTokenFilter

---
+++ reservation-service/src/main/java/com/ftn/reservationservice/filter/AuthenticationTokenFilter.java
@@ -0,0 +1,45 @@
+package com.ftn.reservationservice.filter;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+
+public class AuthenticationTokenFilter extends UsernamePasswordAuthenticationFilter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		String username = httpRequest.getHeader("username");
+		String permissions = httpRequest.getHeader("permissions");
+		
+		if(username != null && permissions != null && !username.equals("") && !permissions.equals("")) {
+			Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+			
+			String[] tokens = permissions.split("\\|");
+			for(String token : tokens) {
+				authorities.add(new SimpleGrantedAuthority(token));
+			}
+			
+			UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(username,  null, authorities);
+			
+			
+			auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpRequest));
+			SecurityContextHolder.getContext().setAuthentication(auth);
+		}
+		
+		chain.doFilter(request, response);
+	}
+	
+}
---
+++ reservation-service/src/main/java/com/ftn/reservationservice/security/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.ftn.reservationservice.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+	securedEnabled = true,
+	jsr250Enabled = true,
+	prePostEnabled = true
+)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    
+    
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+	        .cors()
+	            .and()
+            .headers().frameOptions()
+            	.disable()
+            	.and()
+	        .csrf()
+	            .disable()
+            .x509().subjectPrincipalRegex("CN=(.*?)(?:,|$)")
+	             .and()
+	        .sessionManagement()
+	            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+	            .and()
+	        .authorizeRequests()
+	            .antMatchers("/",
+			                 "/favicon.ico",
+			                 "/**/*.png",
+			                 "/**/*.gif",
+			                 "/**/*.svg",
+			                 "/**/*.jpg",
+			                 "/**/*.html",
+			                 "/**/*.css",
+			                 "/**/*.js")
+	                .permitAll()
+                .antMatchers("/api/**")
+	            	.permitAll();
+//	            .anyRequest()
+//	                .authenticated();
+    }
+	
+}
