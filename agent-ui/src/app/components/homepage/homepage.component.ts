a2a38e45776561324c5639ce9c79c002e267c03e
basicnikola <basic.nikol4@gmail.com>
2019-06-21 16:09:38 +0200

    fix agentui

--- agent-ui/src/app/components/homepage/homepage.component.ts
+++ agent-ui/src/app/components/homepage/homepage.component.ts
@@ -29,7 +29,7 @@ export class HomepageComponent implements OnInit {
           }
         })
       }else{
-        this.authService.signOut();
+        this.authService.logout();
         alert("Nemate rolu agent!");
         this.router.navigate(['login']);
       }
