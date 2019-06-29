5eafd5537e65b0ec96aa0e1db27ce9f0fcb87472
katic7 <katicmilan7@gmail.com>
2019-06-29 11:18:41 +0200

    acl

--- eureka-service/src/main/java/com/ftn/eurekaservice/EurekaServiceApplication.java
+++ eureka-service/src/main/java/com/ftn/eurekaservice/EurekaServiceApplication.java
@@ -1,5 +1,10 @@
 package com.ftn.eurekaservice;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -10,7 +15,15 @@ import org.springframework.context.annotation.Bean;
 public class EurekaServiceApplication {
 
 	public static void main(String[] args) {
-		System.out.println("asdsAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa");
+		
+		File file = new File("/eureka-service/src/main/resources/test.txt");
+		try {
+			BufferedReader bf = new BufferedReader(new FileReader(file));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Nema pristup test.txt fajlu!!");
+		}
+		
 		System.setProperty("KEY_STORE_CLASSPATH", "src/main/resources/eureka.jks");
 		System.setProperty("KEY_STORE_PASSWORD", "password");
 		System.setProperty("KEY_ALIAS", "eureka");
